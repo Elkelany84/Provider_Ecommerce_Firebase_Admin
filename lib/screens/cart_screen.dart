e9055bd9e966b39624e6d4ed0428989395e909ac
Elkelany84 <drnet80@gmail.com>
2024-02-23 15:00:01 +0200

    Part21

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:hadi_ecommerce_firebase_admin/widgets/title_text.dart';
+import 'package:hadi_ecommerce_firebase_admin/services/assets_manager.dart';
+import 'package:hadi_ecommerce_firebase_admin/widgets/empty_bag.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: TitleTextWidget(
-          label: "Cart",
-        ),
-      ),
-    );
+    return Scaffold(
+        body: EmptyBag(
+      imagePath: AssetsManager.shoppingBasket,
+      title: "Whoops",
+      subtile: "Your Cart Is Empty!",
+      details: "Looks Like Your Cart is Empty,Start Shopping!",
+      buttonText: "Shop now",
+    ));
   }
 }
---
+++ lib/widgets/empty_bag.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:hadi_ecommerce_firebase_admin/widgets/subtitle_text.dart';
+import 'package:hadi_ecommerce_firebase_admin/widgets/title_text.dart';
+
+class EmptyBag extends StatelessWidget {
+  const EmptyBag(
+      {super.key,
+      required this.imagePath,
+      required this.title,
+      required this.subtile,
+      required this.details,
+      required this.buttonText});
+  final String title, subtile, details, buttonText, imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          Image.asset(
+            imagePath,
+            width: double.infinity,
+            height: size.height * 0.35,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TitleTextWidget(
+            label: title,
+            fontSize: 40,
+            color: Colors.red,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SubtitleTextWidget(
+            label: subtile,
+            fontWeight: FontWeight.w600,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SubtitleTextWidget(
+              label: details,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {},
+              child: Text(buttonText))
+        ],
+      ),
+    );
+  }
+}
