8a15e35a15db95dc9aec764ff3ec4b89f903a0a1
Elkelany84 <drnet80@gmail.com>
2024-02-24 08:44:34 +0200

    Part25

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:hadi_ecommerce_firebase_admin/services/assets_manager.dart';
+import 'package:hadi_ecommerce_firebase_admin/widgets/app_name_text.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Search")),
+    final TextEditingController controller = TextEditingController();
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(AssetsManager.shoppingCart),
+          ),
+          title: AppNameTextWidget(
+            label: "Search Products",
+            fontSize: 22,
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 2),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/widgets/root_screen.dart
+++ lib/widgets/root_screen.dart
@@ -61,7 +61,11 @@ class _RootScreenState extends State<RootScreen> {
           ),
           NavigationDestination(
             selectedIcon: Icon(IconlyBold.bag2),
-            icon: Icon(IconlyLight.bag2),
+            icon: Badge(
+                backgroundColor: Colors.blue,
+                textColor: Colors.white,
+                label: Text("5"),
+                child: Icon(IconlyLight.bag2)),
             label: "Cart",
           ),
           NavigationDestination(
