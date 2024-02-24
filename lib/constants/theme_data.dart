9bc9fde3f27b3ec69418e67e243355c76d21c768
Elkelany84 <drnet80@gmail.com>
2024-02-24 09:10:02 +0200

    Part26

--- lib/constants/theme_data.dart
+++ lib/constants/theme_data.dart
@@ -5,20 +5,49 @@ class Styles {
   static ThemeData themeData(
       {required bool isDarkTheme, required BuildContext context}) {
     return ThemeData(
-        scaffoldBackgroundColor: isDarkTheme
+      scaffoldBackgroundColor: isDarkTheme
+          ? AppColors.darkScaffoldColor
+          : AppColors.lightScaffoldColor,
+      cardColor: isDarkTheme
+          ? const Color.fromARGB(255, 13, 6, 37)
+          : AppColors.lightCardColor,
+      brightness: isDarkTheme ? Brightness.dark : Brightness.light,
+      appBarTheme: AppBarTheme(
+        iconTheme:
+            IconThemeData(color: isDarkTheme ? Colors.white : Colors.black),
+        backgroundColor: isDarkTheme
             ? AppColors.darkScaffoldColor
             : AppColors.lightScaffoldColor,
-        cardColor: isDarkTheme
-            ? const Color.fromARGB(255, 13, 6, 37)
-            : AppColors.lightCardColor,
-        brightness: isDarkTheme ? Brightness.dark : Brightness.light,
-        appBarTheme: AppBarTheme(
-          iconTheme:
-              IconThemeData(color: isDarkTheme ? Colors.white : Colors.black),
-          backgroundColor: isDarkTheme
-              ? AppColors.darkScaffoldColor
-              : AppColors.lightScaffoldColor,
-          elevation: 0,
-        ));
+        elevation: 0,
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+                width: 1, color: isDarkTheme ? Colors.white : Colors.black),
+            borderRadius: BorderRadius.circular(8)),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 1,
+            color: Colors.transparent,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 1,
+            color: Theme.of(context).colorScheme.error,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 1,
+            color: Theme.of(context).colorScheme.error,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -2,12 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:hadi_ecommerce_firebase_admin/services/assets_manager.dart';
 import 'package:hadi_ecommerce_firebase_admin/widgets/app_name_text.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  late TextEditingController controller;
+
+  @override
+  void initState() {
+    controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final TextEditingController controller = TextEditingController();
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
@@ -30,10 +48,23 @@ class SearchScreen extends StatelessWidget {
               TextField(
                 controller: controller,
                 decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(width: 2),
+                  prefixIcon: Icon(
+                    Icons.search,
+                  ),
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      controller.clear();
+                      FocusScope.of(context).unfocus();
+                    },
+                    child: Icon(
+                      Icons.clear,
+                      color: Colors.red,
+                    ),
                   ),
                 ),
+                onSubmitted: (value) {
+                  controller.text = value;
+                },
               )
             ],
           ),
